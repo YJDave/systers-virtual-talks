c10698a9dccc2b95246a6ac9ab5401c1b6940159
Yashashvi Dave <yashashvidave@gmail.com>
2019-05-22 13:54:37 +0530

    Add basic python syntax.

---
+++ syntax.py
@@ -0,0 +1,27 @@
+### COMMENTS ###
+"""
+    There are two types of comments in python
+    1. Inline comments
+    2. Multiple line comments
+
+    Inline comments starts with '#'
+    Multiple line comments starts and ends with three quotes
+"""
+# NOTE: Single quotes and double quotes are same thing in python
+
+
+### PRINT STATEMENTS ###
+# Print statements are used to display the content in application
+
+# syntax
+print("Hello world!! ")
+print("Welcome to GHC Virtual Talk!!")
+
+
+### INDENTATION ###
+# Indentation is most important factor in Python language. In other programming language
+# indentation do not affect in program compilation.
+
+# ERROR
+#   print("Hello world!!")
+
