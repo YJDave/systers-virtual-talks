dcae5e54138841807826dd08d1509fb9a62e8d56
Yashashvi Dave <yashashvidave@gmail.com>
2019-05-22 15:10:04 +0530

    Add basic variable operations.

---
+++ operations.py
@@ -0,0 +1,51 @@
+### OPERATIONS ON DATA ###
+
+# Numeric values
+no1 = 12
+no2 = 12
+
+print(no1 + no2)
+print(no1 - no2)
+print(no1 * no2)
+
+no1 = 13
+no2 = 5
+
+print("Modulus and division")
+print(no1 // no2)
+print(no1 / no2)
+print(no1 % no2)
+
+print("a ^ b")
+print(2 ** 3)
+
+# In built functions for integer data
+print(abs(-12.121))
+print(int(-12.121))
+print(float(12))
+
+# Textual data
+
+# Concatenate strings
+print("Hello" + " " + "World!")
+
+# Multiply strings
+print("a" * 5)
+
+# In built functions of string
+print(len("123456789"))
+print("abc".upper())
+print("ABC".lower())
+print("   123   ".strip())
+print("abc".replace("a", "z"))
+
+# Boolean data
+
+# `or` `and` are special characters
+print(True or True)
+print(True or False)
+print(False or False)
+
+print(True and False)
+print(True and True)
+print(False and False)
