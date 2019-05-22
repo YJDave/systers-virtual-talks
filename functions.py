8841517a69e1cdb96d1cf4e887e99c2b315ba983
Yashashvi Dave <yashashvidave@gmail.com>
2019-05-22 17:45:53 +0530

    Add functions in Python.

---
+++ functions.py
@@ -0,0 +1,16 @@
+### FUNCTIONS ###
+
+"""
+    Functions are defined using keyword `def`
+    We don't require to define function return data type or
+    argument data type in Python.
+"""
+
+def do_sum(a, b):
+    print(a + b)
+
+def do_multiply(a, b, c):
+    print(a * b * c)
+
+do_sum(1, 1)
+do_multiply(1, 2, 3)
